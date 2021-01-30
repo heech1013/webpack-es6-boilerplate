d2bb7b7ee05d5850af05d67c227e07d6d3fa1a5c
Aiden Shin <heech1013@gmail.com>
2021-01-31 00:51:56 +0900

    create image slider using react-slick (issues on setting img height dynamically)

--- src/pages/MatchingPage/index.tsx
+++ src/pages/MatchingPage/index.tsx
@@ -1,26 +1,89 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import classNames from "classnames/bind";
 import { Button } from "semantic-ui-react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Text from "../../components/Text";
 import styles from "./mobileDefault.module.scss";
+import dummyImage1 from "../../assets/dummy_image_1.png";
 
 const cx = classNames.bind(styles);
 
-const Matching = () => (
-  <div className={cx("container")}>
-    <div className={cx("header")}>
-      <div className={cx("profileWrapper")}>
-        <Button circular icon="user outline" size="large" />
+const Matching = () => {
+  const imageEl = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    if (imageEl.current) {
+      // @ trying several ways to set img height dynamically...
+      // const imageWidth = imageEl.current.offsetWidth;
+      // imageEl.current.height = imageWidth;
+      // imageEl.current.style.height = `${imageWidth}px`;
+      // imageEl.current.setAttribute("style", "height: 414px;");
+    }
+  });
+
+  return (
+    <div className={cx("container")}>
+      <div className={cx("header")}>
+        <div className={cx("profileWrapper")}>
+          <Button circular icon="user outline" size="large" />
+        </div>
+        <div className={cx("logoWrapper")}>
+          <Text text="Title" type="logo" />
+        </div>
+        <div className={cx("helpWrapper")}>
+          <Button circular icon="question" size="large" />
+        </div>
       </div>
-      <div className={cx("logoWrapper")}>
-        <Text text="힙팡" type="logo" />
-      </div>
-      <div className={cx("helpWrapper")}>
-        <Button circular icon="question" size="large" />
-      </div>
+
+      {/* Image slider */}
+      <Slider
+        className={cx("slider")}
+        dots
+        infinite
+        speed={500}
+        slidesToShow={1}
+        slidesToScroll={1}
+      >
+        <img
+          ref={imageEl}
+          className={cx("image")}
+          src={dummyImage1}
+          alt="dummyImage1"
+        />
+        <img
+          ref={imageEl}
+          className={cx("image")}
+          src={dummyImage1}
+          alt="dummyImage2"
+        />
+        <img
+          ref={imageEl}
+          className={cx("image")}
+          src={dummyImage1}
+          alt="dummyImage3"
+        />
+        <img
+          ref={imageEl}
+          className={cx("image")}
+          src={dummyImage1}
+          alt="dummyImage4"
+        />
+        <img
+          ref={imageEl}
+          className={cx("image")}
+          src={dummyImage1}
+          alt="dummyImage5"
+        />
+      </Slider>
+
+      {/* profile */}
+
+      {/* footer */}
     </div>
-  </div>
-);
+  );
+};
 
 export default Matching;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,13 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.tsx",
   output: {
-    filename: "main.js",
+    /** path: a place where bundle files are saved. */
     path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
   devtool: "source-map",
   resolve: {
@@ -19,8 +21,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         /** loader executed sequentially from end of array.
-         * ts-loader should executed first, then babel-loader.
-         */
+         * ts-loader should executed first, then babel-loader. */
         use: ["babel-loader", "ts-loader"],
         include: /src/,
         exclude: /node_modules/,
@@ -33,15 +34,29 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      /** webpack 5 - "Asset Modules"
+       * type "asset/resource": emits a seperate file ans exports the URL. Previously achievable by using 'file-loader'.
+       * type "asset/inline": exports a data URL of the asset. Previously achievable by using 'url-loader'.
+       * type "asset": automatically chooses btwn exporting a data URL and emmitting a separate file.
+         Previously achievable by using 'url-loader' with asset size limit. */
       {
-        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-        loader: "url-loader",
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg|gif)$/i,
+        type: "asset",
       },
     ],
   },
+  plugins: [
+    /** Copies individual files or entire directories, which already exist, to the build directory.
+     * Because webpack doesn't parse HTML, it won't pick up images in <img> tag. */
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "" }],
+    }),
+  ],
   // webpack-dev-server setting
   devServer: {
     contentBase: "./",
     publicPath: "/dist",
+    /** writeToDisk: let webpack-dev-server to write files to the output directory during development. */
+    writeToDisk: true,
   },
 };
